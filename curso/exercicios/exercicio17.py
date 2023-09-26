0b97869ca2580d9b72fcb05b724e30ed5adaf41e
Noobpro112 <reismatheusyan83@gmail.com>
2023-09-26 15:48:26 -0300

    exer

--- curso/exercicios/exercicio17.py
+++ curso/exercicios/exercicio17.py
@@ -1,4 +1,6 @@
+import math
 co=int(input("Qual o seu cateto oposto?"))
 catad=int(input("Qual o seu cateto adjacente?"))
-hipotenusa=(co**2+catad**2)**0.5
-print('A hipotenusa vai medir {:.1f}'.format(hipotenusa))
+hipotenusa=(co**2+catad**2)
+h=math.sqrt(hipotenusa)
+print('A hipotenusa vai medir {:.1f}'.format(h))
---
+++ curso/exercicios/exercicio18.py
@@ -0,0 +1,7 @@
+caop=float(input('Qual seu cateto oposto?'))
+caad=float(input('Qual seu cateto adjascente?'))
+hipotenusa=float(input('Qual sua hipotenusa?'))
+seno=caop/hipotenusa
+cosseno=caad/hipotenusa
+tangente=caop/caad
+print('O seu seno é {} o seu cosseno é {} e a sua tangente é {}'.format(seno,cosseno,tangente))
