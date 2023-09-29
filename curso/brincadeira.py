4cdd8caab518c768d76391a98abd430182990b10
Matheus Yan dos Reis <reismatheusyan83@gmail.com>
2023-09-29 11:28:47 -0300

    a

--- curso/brincadeira.py
+++ curso/brincadeira.py
@@ -1,15 +1,19 @@
+import os
 from selenium import webdriver
 import time
 
+# Adicione o caminho do driver ao PATH
+os.environ['PATH'] += os.pathsep + 'C:\\Drivers'
+
 # Crie uma nova instância do navegador
-driver = webdriver.Edge(executable_path='C:\\Drivers\\msedgedriver.exe')
+driver = webdriver.Edge()
 
 # Vá para a página da web que você quer
 driver.get('https://github.com/Noobpro112')
 
 while True:
     # Espere 5 segundos
-    time.sleep(10)
+    time.sleep(5)
     
     # Recarregue a página
     driver.refresh()
