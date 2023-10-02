cc379f88716e773d43c8109aae8f5c0d88d1fc8f
Matheus Yan dos Reis <reismatheusyan83@gmail.com>
2023-10-02 12:22:54 -0300

    -

---
+++ curso/exercicios/exercicio29.py
@@ -0,0 +1,5 @@
+km=float(input('Quantos Km/h o carro estava andando?'))
+if km>= 80 :
+    print('O carro foi multado em {} reais'.format(km*7.0))
+else:
+    print('O carro está dentro dos limites.')
