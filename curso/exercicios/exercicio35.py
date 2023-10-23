6b239e13124ae62707b569bb8e9e7644909554b7
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-10-23 09:18:03 -0300

    -

--- curso/exercicios/exercicio35.py
+++ curso/exercicios/exercicio35.py
@@ -1,5 +1,11 @@
-lados=int(input('Quantas barras você tem?'))
-if lados == 3 :
-    print('você consegue fazer um triangulo.')
+print('-=' * 20) 
+print('ANALISADOR DE TRIÂNGULOS')
+print('-=' * 20) 
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    decisão = 'PODEM'
 else:
-    print('Você não consegue fazer um triangulo.')
+    decisão = 'NÃO PODEM'   
+print(f'As retas {r1}, {r2}, e {r3} {decisão} formar um triângulo')
---
+++ curso/exercicios/exercicio41.py
@@ -0,0 +1,11 @@
+idade=int(input("Qual a idade do atleta?"))
+if idade<=9:
+    print("MIRIM")
+elif idade <=14:
+    print("INFANTIL")
+elif idade <=19:
+    print("JUNIOR")
+elif idade <= 20 :
+    print("SENIOR")
+else:
+    print("MASTER")
