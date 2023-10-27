29096db852904d9d13b03378ac5c1f348c71b80b
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-10-27 11:11:47 -0300

    Sofri nesse viu rapaz

--- curso/exercicios/exercicio52.py
+++ curso/exercicios/exercicio52.py
@@ -1,1 +1,6 @@
-pala=str(len(input("Sua palavra é um palindromo?")))
+frase=str(input("Sua palavra é um palindromo? "))
+frase1=frase.replace(" ","").lower()
+if frase1 == frase1[::-1]:
+    print("É Um palindromo.")
+else:
+    print("Não é um palindromo")
