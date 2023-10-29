566786920c384236949d06b3570d43553bc6899c
Matheus Yan <reismatheusyan83@gmail.com>
2023-10-29 01:39:42 -0300

    Update exercicio62.py

--- curso/exercicios (do 57 em diante)/exercicio62.py
+++ curso/exercicios (do 57 em diante)/exercicio62.py
@@ -2,20 +2,19 @@ print("Vamos ver uma Razão aritmétria")
 r=int(input("Qual a sua razão? "))
 a1=int(input("Qual o seu primeiro termo? "))
 c=int(input("Quantos termos você quer? "))
-while True:
+c1=c
+while c>1:
     while c >1 :
-        if c == 0:
-            print("Adeus Não existe PA!")
-            pass
         an =a1+(c - 1)*r
+        if c1==c:
+            an_quantidade=an
+            print(c1)
         c-=1
         print("A sequencia de numeros de sua PA pela quantidade de termos escolhidos por você é : {}".format(an))
     escolha = str(input("Quer mostrar mais termos? [Y/N]")).upper()
     if escolha == "Y":
         c=int(input("Mais quantas vezes?"))
-        if c == 0:
-            print("Adeus Não existe PA!")
-            pass
-    if escolha=="N":
+        a1=an_quantidade
         pass
-    pass
+    if escolha=="N":
+        break
