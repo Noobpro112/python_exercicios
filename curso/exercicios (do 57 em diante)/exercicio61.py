d1d9dc3829f3b444cc758353c2d879bdc882772c
Matheus Yan <reismatheusyan83@gmail.com>
2023-10-29 01:05:29 -0300

    -

--- curso/exercicios (do 57 em diante)/exercicio61.py
+++ curso/exercicios (do 57 em diante)/exercicio61.py
@@ -1,11 +1,9 @@
 print("Vamos ver uma Razão aritmétria")
 r=int(input("Qual a sua razão? "))
 a1=int(input("Qual o seu primeiro termo? "))
-c=int(input("Quantos termos você quer? "))
+c=10
 while c >1 :
-    if c == 0:
-        print("Adeus Não existe PA!")
-        break
     an =a1+(c - 1)*r
     c-=1
-    print("A sequencia de numeros de sua PA pela quantidade de termos escolhidos por você é : {}".format(an))
+    print("A sua progressão aritmétrica é : {}".format(an))
+print("Sua PA é essa!")
---
+++ curso/exercicios (do 57 em diante)/exercicio62.py
@@ -0,0 +1,11 @@
+print("Vamos ver uma Razão aritmétria")
+r=int(input("Qual a sua razão? "))
+a1=int(input("Qual o seu primeiro termo? "))
+c=int(input("Quantos termos você quer? "))
+while c >1 :
+    if c == 0:
+        print("Adeus Não existe PA!")
+        break
+    an =a1+(c - 1)*r
+    c-=1
+    print("A sequencia de numeros de sua PA pela quantidade de termos escolhidos por você é : {}".format(an))   
