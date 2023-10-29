2276efe8c152d341b6df82745fb19201c709f1f2
Matheus Yan <reismatheusyan83@gmail.com>
2023-10-28 22:32:09 -0300

    desnecessario
    Nunca passei tanto sufoco em algo que era pra ser incrivelmente facil.

---
+++ curso/exercicios (do 57 em diante)/exercicio60.py
@@ -0,0 +1,13 @@
+n1 = int(input("Escolha um número para calcular o fatorial: "))
+fatorial_resultado = 1
+fatorial = n1
+
+while fatorial > 1:
+    fatorial_resultado *= fatorial
+    fatorial -= 1
+
+print("O resultado da sua fatorial é {}".format(fatorial_resultado))    
+
+
+
+
