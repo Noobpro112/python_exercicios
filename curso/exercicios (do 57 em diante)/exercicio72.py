54a65f148503b88741a23f20fc9e566a20fcb9a5
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-11-10 09:13:37 -0300

    Create exercicio72.py

---
+++ curso/exercicios (do 57 em diante)/exercicio72.py
@@ -0,0 +1,70 @@
+tupla=('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito','nove','dez','onze','doze','treze (L)','quatorze','quinze','dezesseis', 'dezessete', 'dezoito', 'dezenove','vinte')
+
+while True:
+    escolha=int(input("Digite um numero de 1 a 20: "))
+    if escolha<=20:
+        if escolha == 0:
+            print(f'Seu numero é {tupla[0]}')
+            break
+        if escolha == 1:
+            print(f'Seu numero é {tupla[1]}')
+            break
+        if escolha == 2:
+            print(f'Seu numero é {tupla[2]}')
+            break
+        if escolha == 3:
+            print(f'Seu numero é {tupla[3]}')
+            break
+        if escolha == 4:
+            print(f'Seu numero é {tupla[4]}')
+            break
+        if escolha == 5:
+            print(f'Seu numero é {tupla[5]}')
+            break
+        if escolha == 6:
+            print(f'Seu numero é {tupla[6]}')
+            break
+        if escolha == 7:
+            print(f'Seu numero é {tupla[7]}')
+            break
+        if escolha == 8:
+            print(f'Seu numero é {tupla[8]}')
+            break
+        if escolha == 9:
+            print(f'Seu numero é {tupla[9]}')
+            break
+        if escolha == 10:
+            print(f'Seu numero é {tupla[1]}')
+            break
+        if escolha == 11:
+            print(f'Seu numero é {tupla[10]}')
+            break
+        if escolha == 12:
+            print(f'Seu numero é {tupla[11]}')
+            break
+        if escolha == 13:
+            print(f'Seu numero é {tupla[12]}') 
+            break
+        if escolha == 14:
+            print(f'Seu numero é {tupla[13]}') 
+            break
+        if escolha == 15:
+            print(f'Seu numero é {tupla[14]}') 
+            break
+        if escolha == 16:
+            print(f'Seu numero é {tupla[15]}') 
+            break
+        if escolha == 17:
+            print(f'Seu numero é {tupla[16]}') 
+            break
+        if escolha == 18:
+            print(f'Seu numero é {tupla[17]}')
+            break 
+        if escolha == 19:
+            print(f'Seu numero é 1{tupla[8]}') 
+            break
+        if escolha == 20:
+            print(f'Seu numero é {tupla[9]}')
+            break
+    else: 
+        print("Numero invalido.")
