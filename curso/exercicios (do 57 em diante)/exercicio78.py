cd0c22e535a801d86d672e9160da0f99dda85f72
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-11-13 07:24:58 -0300

    Create exercicio78.py

---
+++ curso/exercicios (do 57 em diante)/exercicio78.py
@@ -0,0 +1,5 @@
+lista=[int(input('Digite um numero: ')),int(input('Digite um numero: ')),int(input('Digite um numero: ')),int(input('Digite um numero: ')),int(input('Digite um numero: '))]
+
+lista_ordenada=list.sort()
+
+print(f'Você digitou os seguintes numeros: {lista}, Seu menor numero é {lista_ordenada[0]} e o maior é {lista_ordenada[-1]}')
