cecc7dd91921422108d74796737dab3554b7b7a9
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-11-20 08:35:31 -0300

    Create exercicio86.py

---
+++ curso/exercicios (do 57 em diante)/exercicio86.py
@@ -0,0 +1,7 @@
+matriz=[[ 0, 0, 0],[ 0, 0, 0],[ 0, 0, 0]]
+for l in range(0,3):
+    for c in range(1,4) :
+        matriz[l][c]=int(input(f'Digite um valor: '))
+print('-='*30)
+print('Acabou')
+print(matriz)
