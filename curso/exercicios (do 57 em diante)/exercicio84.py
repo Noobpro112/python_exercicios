02ce9a832b1931d8ee948e338e155110ea95dc94
MATHEUS YAN DOS REIS <reismatheusyan83@gmail.com>
2023-11-20 07:47:18 -0300

    Update exercicio84.py

--- curso/exercicios (do 57 em diante)/exercicio84.py
+++ curso/exercicios (do 57 em diante)/exercicio84.py
@@ -1,6 +1,7 @@
-pessoa=list()
-dados=list()
-quantidade=0
+pessoa = list()
+dados = list()
+quantidade = 0
+
 while True:
     pessoa.append(str(input('Digite o seu nome: ')))
     if pessoa[0] == 'sair':
@@ -8,6 +9,8 @@ while True:
     pessoa.append(int(input('Digite o seu peso: ')))
     dados.append(pessoa[:])
     pessoa.clear()
-    quantidade+=1
+    quantidade += 1
+
 dados.sort()
-print('A pessoa mais pesada tem {} kgs a mais leve tem {} kgs e foram cadastradas {} pessoas.'.format(dados[-1,1], dados[0,1], quantidade))
+
+print(f'A pessoa mais pesada tem {dados[-1][1]} kgs, a mais leve tem {dados[0][1]} kgs e foram cadastradas {quantidade} pessoas.')
